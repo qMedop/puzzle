ea2e43815634d64d196365331e1b0389975adb95
Mohamed Saleh <95769954+qMedop@users.noreply.github.com>
2024-09-01 17:46:06 +0300

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,6 +1,6 @@
 let grid = 9;
 const board = document.querySelector("#board");
-let imageSrc = "img/puzzle-image/main2.jpg";
+let imageSrc = "img/main.jpg";
 
 class puzzleGame {
   constructor(board, grid, imgSrc) {
